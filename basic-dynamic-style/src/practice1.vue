9e19ff25bb76e0ab5303fd7a8a41de44badc6283
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2022-02-14 23:44:27 +0700

    add

---
+++ basic-dynamic-style/src/practice1.vue
@@ -0,0 +1,51 @@
+<script setup>
+const accounts = [
+  {
+    name: 'salary',
+    amount: 10000
+  },
+  {
+    name: 'rent',
+    amount: -2500
+  },
+  {
+    name: 'parking fee',
+    amount: 0
+  },
+  {
+    name: 'car fuel',
+    amount: -600
+  },
+  {
+    name: 'commission',
+    amount: 5500
+  }
+]
+</script>
+
+<template>
+  <ul>
+    <li v-for="account in accounts">
+      <p v-if="account.amount !== 0">
+        <span> {{ account.name }} </span>:
+        <span
+          :style="
+            account.amount > 0
+              ? 'background-color:green'
+              : 'background-color:red'
+          "
+        >
+          {{ account.amount }}</span
+        >
+      </p>
+    </li>
+  </ul>
+  <p>
+    Net Total:
+    {{ accounts.reduce((total, account) => total + account.amount, 0) }}
+  </p>
+  <!-- #round 1 (0, account#1) :   0+10000
+      #round 2 (10000, account#2): 10000+(-2500)
+      #round 3(7500, account#3):....
+  -->
+</template>
