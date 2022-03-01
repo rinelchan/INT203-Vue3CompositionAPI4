76567c6ddbfb776eb5011ca974938e359639522d
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2022-03-01 20:15:57 +0700

    add

--- basic-dynamic-style/src/App.vue
+++ basic-dynamic-style/src/App.vue
@@ -1,24 +1,96 @@
 <script setup>
-const isError = false
-const decorText = true
-const a = 'errorMsg'
-const b = 'textUnderline'
+import { reactive, ref, computed } from "vue";
+const cardList = reactive([]);
+for (let i = 0; i < 12; i++) {
+  cardList.push({
+    value: i,
+    visible: false,
+    position: i,
+    matched: false
+  });
+}
+
+const shuffleCard = () => {
+  cardList.value = _.shuffle(cardList.value)
+}
+
+const restart = () => {
+  shuffleCard()
+
+  cardList.value = cardList.value.map(card => {
+    return {
+      ...card,
+      matched: false,
+      position: index,
+      visible: false
+    }
+  })
+}
+
+
+
+// true = แดง / false = น้ำเงิน ได้แหละ
+// const visible = {
+//   type: Boolean,
+//   default: false,
+// };
+
+const flipCard = (index) => {
+  cardList[index].visible = true;
+};
 </script>
- 
 <template>
-  <p :class="[a, decorText ? b : '']">this is example text</p>
-  <p :class="isError ? [a, b] : a">XXXXX</p>
-  <p :class="isError ? [a, b] : ''">YYYY</p>
+  <section class="game-board">
+    <div
+      class="card"
+      v-for="(card, index) in cardList"
+      :key="`card-${index}`"
+      :value="card.value"
+      :visible="card.visible"
+      :position="card.position"
+      @click="flipCard(index)"
+    >
+      <div v-if="cardList[index].visible" class="card-face is-front">
+        {{ index }}
+      </div>
+      <div v-else class="card-face is-back">Back</div>
+    </div>
+  </section>
+
+
+
+<button @click="restart">RESTART GAME</button>
+
 </template>
- 
+
 <style>
-.errorMsg {
-  color: red;
+.card {
+  border: 5px solid#ccc;
+  position: relative;
 }
-.normalMsg {
-  color: green;
+.card-face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
 }
-.textUnderline {
-  text-decoration: underline;
+
+.card-face.is-front {
+  background-color: red;
+  color: white;
+}
+.card-face.is-black {
+  background-color: blue;
+  color: white;
+}
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
 }
 </style>
+
+
+
---
+++ basic-dynamic-style/src/EXAM 1.vue
@@ -0,0 +1,104 @@
+<script setup>
+import { ref } from 'vue'
+const id = 63130500035
+const name = 'Nutwara Boonyangkongkaew'
+
+const italicText = 'italic'
+const isItalic = false
+const propItalic = 'font-style:italic'
+
+
+const randNumbers = ref([])
+// generates random number in range (1-99)
+const generateNumber = () => {
+  let duplicate = false
+  const rand = Math.floor(Math.random() * 99 + 1)
+  duplicate = randNumbers.value.some((randNumber) => randNumber === rand)
+  duplicate ? generateNumber() : randNumbers.value.push(rand)
+}
+</script>
+
+<template>
+
+  <p :style="{color:invalidMessage, 'font-style': italicText}">
+    {{ id }}
+  </p>
+  <p :style="{ color: isItalic ? 'red' : 'green' }">
+    {{ name }}
+  </p>
+
+  <div class="top-container">
+    <div class="action-button">
+      <button @click="generateNumber">Generate New Number</button>
+      <button @click="reset">Reset</button>
+    </div>
+
+    <div class="error-message">
+      <!-- show error message when a user adding the same number in the bingo sheet -->
+      <p :class="[duplicate ? rand : '']">Sorry, Your number is already exist in the bingo sheet, please generate new number again!</p>
+    </div>
+    
+    <div class="grid-container">
+      <div class="grid-item">
+        <!-- show bingo 3 rows x 3 cols here -->
+      
+        <span class="grid-item">Add Number</span>
+        <span class="grid-item">Add Number</span>
+        <span class="grid-item">Add Number</span>
+
+        <span class="grid-item">Add Number</span>
+        <span class="grid-item">Add Number</span>
+        <span class="grid-item">Add Number</span>
+        
+        <span class="grid-item">Add Number</span>
+        <span class="grid-item">Add Number</span>
+        <span class="grid-item">Add Number</span>
+
+      </div>
+    </div>
+
+    <div class="generate-number">
+      <!-- show the list of generated number here -->
+      <li>generateNumber #1: {{ randNumbers }}</li>
+      <li>generateNumber #2: {{ randNumbers }}</li> 
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.top-container {
+  width: 330px;
+}
+.action-button {
+  padding: 10px;
+  display: flex;
+  gap: 5px;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: 100px 100px 100px;
+  grid-template-rows: 80px 80px 80px;
+  column-gap: 5px;
+  row-gap: 5px;
+  background-color: green;
+  padding: 10px;
+}
+.grid-item {
+  display: flex;
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  font-size: 30px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.generate-number {
+  font-size: 20px;
+}
+.error-message {
+  height: 40px;
+  color: red;
+}
+</style>
